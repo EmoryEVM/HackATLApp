8a614b2810f69c30f12c4a0a5631c9f44a63be3b
corbinbalzan <corbinbalzan@gmail.com>
2018-09-23 19:24:21 -0400

    Test updates to the main screen

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ import {name as appName} from './app.json';
 import {Button, AppRegistry, Platform, StyleSheet, Text, View} from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+  D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
@@ -27,6 +27,7 @@ export default class App extends Component {
           // }
         >
         </Button>
+        <Text> Test of Text </Text>
       </View>
     );
   }
