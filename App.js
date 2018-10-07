a962bafbdce51b90f50d07058b5d619a29196a16
AndrewBantug <AndrewBantug@gmail.com>
2018-10-07 18:03:56 -0400

    logo and date text

--- App.js
+++ App.js
@@ -6,39 +6,45 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {StackNavigator } from 'react-navigation';
-import {Button, AppRegistry, Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react'
+import { StackNavigator } from 'react-navigation'
+import {
+  Button,
+  AppRegistry,
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native'
 
-import Home from './src/screens/Home';
-import Agenda from './src/screens/Agenda';
-import Faq from './src/screens/Faq';
-import Inbox from './src/screens/Inbox';
-import Map from './src/screens/Map';
-import Pitch from './src/screens/Pitch';
+import Home from './src/screens/Home'
+import Agenda from './src/screens/Agenda'
+import Faq from './src/screens/Faq'
+import Inbox from './src/screens/Inbox'
+import Map from './src/screens/Map'
+import Pitch from './src/screens/Pitch'
 
-
-//This is the first page the app goes to, but this holds all the screens
-//and then navigates to the first one
+// This is the first page the app goes to, but this holds all the screens
+// and then navigates to the first one
 const Navigation = StackNavigator({
-  Home:{
-    screen:Home
+  Home: {
+    screen: Home
   },
-  Agenda:{
-    screen:Agenda
+  Agenda: {
+    screen: Agenda
   },
-  Faq:{
-    screen:Faq
+  Faq: {
+    screen: Faq
   },
-  Inbox:{
-    screen:Inbox
+  Inbox: {
+    screen: Inbox
   },
-  Map:{
-    screen:Map
+  Map: {
+    screen: Map
   },
-  Pitch:{
-    screen:Pitch
+  Pitch: {
+    screen: Pitch
   }
-}) 
+})
 
 export default Navigation
