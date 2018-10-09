e153b3d40930fc17023a6f280f43941aa1d14d51
apolanco18 <39174984+apolanco18@users.noreply.github.com>
2018-10-08 23:11:45 -0400

    NavBar
    created the basic navbar but havent added the icons

--- App.js
+++ App.js
@@ -7,8 +7,9 @@
  */
 
 import React, {Component} from 'react';
-import {StackNavigator } from 'react-navigation';
-import {Button, AppRegistry, Platform, StyleSheet, Text, View} from 'react-native';
+import {createBottomTabNavigator} from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 
 import Home from './src/screens/Home';
 import Agenda from './src/screens/Agenda';
@@ -20,25 +21,50 @@ import Pitch from './src/screens/Pitch';
 
 //This is the first page the app goes to, but this holds all the screens
 //and then navigates to the first one
-const Navigation = StackNavigator({
-  Home:{
-    screen:Home
+const Navigation = createBottomTabNavigator(
+  {
+    Home:{
+      screen:Home
+    },
+    Agenda:{
+      screen:Agenda
+    },
+    Faq:{
+      screen:Faq
+    },
+    // Inbox:{
+    //   screen:Inbox
+    // },
+    Map:{
+      screen:Map
+    },
+    Pitch:{
+      screen:Pitch
+    },
   },
-  Agenda:{
-    screen:Agenda
-  },
-  Faq:{
-    screen:Faq
-  },
-  Inbox:{
-    screen:Inbox
-  },
-  Map:{
-    screen:Map
-  },
-  Pitch:{
-    screen:Pitch
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+        } else if (routeName === 'Agenda') {
+          iconName = `ios-options${focused ? '' : '-outline'}`;
+        }
+
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return <Ionicons name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
+    animationEnabled: false,
+    swipeEnabled: false,
   }
-}) 
+)
 
 export default Navigation
